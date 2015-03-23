c2de46ab91ce93dc51fef6676b723509bc3a5402
Lastdates <deatul@gmail.com>
2015-03-23 16:26:46 +0530

    Update jquery.tabClick.js

--- jquery.tabClick.js
+++ jquery.tabClick.js
@@ -9,12 +9,8 @@
 * Date: Thu Mar 19 2015 02:29:11 GMT+0530 (IST)
 */
 (function($){
-	var T={},f,
-	click=function(p){
-		f(p.data[0],p.data[1]);
-		return false;
-	};
-	$.fn.tabClick=function(name,x){
+	var T={},
+	f=function(name,x){
 		var e=T[name],self=0,t=e.a;
 		if(x > -1 && x < e.l){
 			if(e.d.eq(x).css('display')!='none')self=1;
@@ -39,8 +35,11 @@
 			e.h.addClass('active');
 			e.d.show();
 		}
+	},
+	c=function(p){
+		f(p.data[0],p.data[1]);
+		return false;
 	};
-	f=$().tabClick;
 	$.fn.tabClickInit=function(name,heads,data,type,tabopen,hover,keyboard,callback){
 		T[name]={t:type,h:$(heads),d:$(data),a:-1,c:callback};
 		var e=T[name],h=e.h,t;
@@ -48,7 +47,7 @@
 		e.d.hide();
 		h.removeClass('active')
 			.each(function(i){
-				$(this).on("click",[name,i],click);
+				$(this).on("click",[name,i],c);
 			});
 		if('ontouchstart' in window)
 			h.each(function(i){
@@ -67,11 +66,11 @@
 		if(hover)
 			(hover == 1) ?
 				h.each(function(i){
-					$(this).on("mouseover",[name,i],function(p){t=setTimeout(function(){click(p)},380);});
+					$(this).on("mouseover",[name,i],function(p){t=setTimeout(function(){c(p)},380);});
 					$(this).on("mouseout",function(){clearTimeout(t);});
 				}) :
 				h.each(function(i){
-					$(this).on("mouseover",[name,i],click);
+					$(this).on("mouseover",[name,i],c);
 				});
 		if(keyboard && type!=2)
 			$(document).on("keydown",function(E){
@@ -81,4 +80,5 @@
 		for(t=0;t<tabopen.length;t++)
 			f(name,tabopen[t]);
 	};
+	$.fn.tabClick=f;
 })(jQuery);
